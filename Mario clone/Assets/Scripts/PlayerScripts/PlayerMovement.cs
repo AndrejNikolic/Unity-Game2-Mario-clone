023e149e1a9341860a661e04607a2ebf22b02422
Andrej Nikolić <existence.of.utopia@gmail.com>
2020-02-08 14:30:24 +0100

    Player
    Added player, player movement, animations and directions

---
+++ Mario clone/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // called every few frames, change it in Edit > Project Settings > Time > Fixed Timestep
+    private void FixedUpdate()
+    {
+        PlayerWalk();
+    }
+
+    void PlayerWalk()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); // prints whole number for left and right (-1 and 1)
+        if (h==1)
+        {
+            myBody.velocity = new Vector2(speed, myBody.velocity.y); // velocity - speed over time
+            ChangeDirection(1);
+
+        } else if (h == -1)
+        {
+            myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+            ChangeDirection(-1);
+        } else
+        {
+            myBody.velocity = new Vector2(0f, myBody.velocity.y);
+        }
+
+        anim.SetInteger("Speed", Mathf.Abs((int)myBody.velocity.x));
+    }
+
+    void ChangeDirection(int direction)
+    {
+        Vector3 tempScale = transform.localScale;
+        tempScale.x = direction;
+        transform.localScale = tempScale;
+    }
+
+}// end PlayerMovement 
