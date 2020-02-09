e37d069c89c4888bb10740e5b27f2fc0f0da738f
Andrej Nikolić <existence.of.utopia@gmail.com>
2020-02-09 12:15:18 +0100

    Snail interactions with player

--- Mario clone/Assets/Scripts/EnemiesScript/SnailScript.cs
+++ Mario clone/Assets/Scripts/EnemiesScript/SnailScript.cs
@@ -8,37 +8,107 @@ public class SnailScript : MonoBehaviour
     private Rigidbody2D myBody;
     private Animator anim;
 
+    public LayerMask playerLayer;
+
     private bool moveLeft;
 
-    public Transform down_Collision;
+    private bool canMove;
+    private bool stunned;
+
+    public Transform left_Collision, right_Collision, top_Collision, down_Collision;
+    private Vector3 left_Collision_Position, right_Collision_Position;
 
     private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        left_Collision_Position = left_Collision.position;
+        right_Collision_Position = right_Collision.position;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         moveLeft = true;
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moveLeft)
+        if (canMove)
         {
-            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
-        } else
-        {
-            myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+            if (moveLeft)
+            {
+                myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+            }
+            else
+            {
+                myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+            }
         }
+        
         CheckCollision();
     }
 
     void CheckCollision()
     {
+        RaycastHit2D leftHit = Physics2D.Raycast(left_Collision.position, Vector2.left, 0.1f, playerLayer);
+        RaycastHit2D rightHit = Physics2D.Raycast(right_Collision.position, Vector2.right, 0.1f, playerLayer);
+
+        Collider2D topHit = Physics2D.OverlapCircle(top_Collision.position, 0.2f, playerLayer);
+
+        if (topHit != null)
+        {
+            if (topHit.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    // Bounce off player when snail is stunned
+                    topHit.gameObject.GetComponent<Rigidbody2D>().velocity = 
+                        new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+
+                    canMove = false;
+                    myBody.velocity = new Vector2(0, 0);
+                    anim.Play("SnailStuned");
+                    stunned = true;
+                } else
+                {
+                    myBody.velocity = new Vector2(15f, myBody.velocity.y);
+                }
+            }
+        }
+
+        if (leftHit)
+        {
+            if (leftHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    //Apply damage to player
+                }
+                else
+                {
+                    myBody.velocity = new Vector2(15f, myBody.velocity.y);
+                }
+            }
+        }
+        if (rightHit)
+        {
+            if (rightHit.collider.gameObject.tag == MyTags.PLAYER_TAG)
+            {
+                if (!stunned)
+                {
+                    //Apply damage to player
+                }
+                else
+                {
+                    myBody.velocity = new Vector2(-15f, myBody.velocity.y);
+                }
+            }
+        }
+
         // Testing if we don't detect collision
         if (!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f))
         {
@@ -54,13 +124,22 @@ public class SnailScript : MonoBehaviour
         if (moveLeft)
         {
             tempScale.x = Mathf.Abs(tempScale.x);
+            left_Collision.position = left_Collision_Position;
+            right_Collision.position = right_Collision_Position;
         }
         else
         {
             tempScale.x = -Mathf.Abs(tempScale.x);
+            left_Collision.position = right_Collision_Position;
+            right_Collision.position = left_Collision_Position;
         }
 
         transform.localScale = tempScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
     }
 
 } // end SnailScript
---
+++ Mario clone/Assets/Scripts/HelpScripts/MyTags.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTags : MonoBehaviour
+{
+    public static string PLAYER_TAG = "Player";
+}
