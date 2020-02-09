841720237da318b0e4ec096fec44a71b21dedd74
Andrej Nikolić <existence.of.utopia@gmail.com>
2020-02-09 14:07:33 +0100

    Player is shooting

---
+++ Mario clone/Assets/Scripts/PlayerScripts/FireBullet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    private float speed = 10f;
+    private Animator anim;
+
+    private bool canMove;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove = true;
+        StartCoroutine(DisableBullet(5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        if (canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime; //__ * Time.deltaTime used for smoother movement of GameObject
+            transform.position = temp;
+        }
+        
+    }
+
+    public float Speed { get => speed; set => speed = value; }
+
+    IEnumerator DisableBullet(float time)
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == MyTags.BEETLE_TAG || collision.gameObject.name == "Snail")
+        {
+            anim.Play("Explode");
+            canMove = false;
+            StartCoroutine(DisableBullet(0.2f));
+        }
+    }
+
+}
---
+++ Mario clone/Assets/Scripts/PlayerScripts/PlayerShootScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootScript : MonoBehaviour
+{
+    public GameObject fireBullet;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootBullet();
+    }
+
+    void ShootBullet()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            GameObject bullet = Instantiate(fireBullet, transform.position, Quaternion.identity);
+            bullet.GetComponent<FireBullet>().Speed *= transform.localScale.x;
+        }
+    }
+
+} //end PlayerShootScript
