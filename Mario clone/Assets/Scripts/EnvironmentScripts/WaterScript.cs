a978fecedb37ae256ce210281db511526dc37a42
Andrej Nikolić <existence.of.utopia@gmail.com>
2020-02-19 11:15:06 +0100

    Restart game when in water

---
+++ Mario clone/Assets/Scripts/EnvironmentScripts/WaterScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == MyTags.PLAYER_TAG)
+        {
+            StartCoroutine(RestartGame());
+        }
+    }
+
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Gameplay");
+    }
+}
